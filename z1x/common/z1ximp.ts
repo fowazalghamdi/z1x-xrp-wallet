52c6dd43c48c0c38fcdb5fe41674bb59314da7cd
f1f47a23 <109278574+f1f47a23@users.noreply.github.com>
2022-08-02 18:23:46 +0200

    Update z1ximp.ts

--- z1x/common/z1ximp.ts
+++ z1x/common/z1ximp.ts
@@ -1,4 +1,4 @@
-//import * as xrpl from "xrpl"; // mainnet/testnet/devnet
+//import * as xrpl from "xrpl"; // mainnet,testnet,devnet
 import * as xrpl from "xrpl-hooks"; // hooks-testnet
 export { xrpl };
 
